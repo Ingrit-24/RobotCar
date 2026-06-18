47c5140345109472e2472ee073c0fbd7ffbe80c7
Ingrit-24 <shogo.taki.2402@icloud.com>
2026-06-18 19:34:35 +0900

    kouin

--- launch/RTK.launch.py
+++ launch/RTK.launch.py
@@ -8,7 +8,7 @@ from launch_ros.actions import Node
 def generate_launch_description():
     external_pkg_share = get_package_share_directory('gnss_ros_standardization')
     yaml_file_path = os.path.join(external_pkg_share, 'config', 'real_time_kinematic.yaml')
-    yaml_file_path2 = os.path.join(external_pkg_share, 'config', 'ubx_driver.yaml')
+    ubx_file2 = os.path.join(external_pkg_share, 'config', 'ubx_driver.yaml')
 
     user_name_arg = DeclareLaunchArgument(
         'user_name',
@@ -18,10 +18,10 @@ def generate_launch_description():
         'password',
         description='Password for the streaming service'
     )
-    
     ntrip = PythonExpression([
         "'ntrip://' + '", LaunchConfiguration('user_name'), "' + ':' + '", LaunchConfiguration('password'), "' + '@citgnss.taroz.net:2101/RTCM3'"
     ])
+
     return LaunchDescription([
         user_name_arg,
         password_arg,
@@ -34,8 +34,8 @@ def generate_launch_description():
             parameters=[yaml_file_path]
         ),
         Node(
-            package='gnss_ros_standardization',    
-            executable='rtcm_decoder_node', 
+            package='gnss_ros_standardization',
+            executable='rtcm_decoder_node',
             name='getdata',
             output='screen',
             parameters=[{
@@ -43,10 +43,10 @@ def generate_launch_description():
             }]
         ),
         Node(
-            package='gnss_ros_standardization',    
-            executable='ubx_driver_node', 
+            package='gnss_ros_standardization',
+            executable='ubx_driver_node',
             name='getdata',
             output='screen',
-            parameters=[]
+            parameters=[ubx_file2]
         ),
     ])
